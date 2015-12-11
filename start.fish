96eac3db377b5c5d2cdf947f28d41a8e78710ad8
fiatjaf@spooner <fiatjaf@gmail.com>
2015-12-11 14:42:45 -0500

    vundle init on start script.

--- start.fish
+++ start.fish
@@ -8,3 +8,8 @@ ln -sf "$here/mailcap" $HOME/.mailcap
 ln -sf "$here/muttrc" $HOME/.muttrc
 ln -sf "$here/ranger-rc.conf" $HOME/.config/ranger/rc.conf
 ln -sf "$here/ranger-scope.sh" $HOME/.config/ranger/scope.sh
+
+if not test -d ~/.vim/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+end
