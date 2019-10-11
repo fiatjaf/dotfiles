373931b0ce9326a8789975964c8ff9ec48bddc17
fiatjaf <fiatjaf@gmail.com>
2019-10-11 13:40:19 +0000

    remove ranger stuff

--- start.fish
+++ start.fish
@@ -4,12 +4,6 @@ ln -sfT "$here/vim" $HOME/.vim
 ln -sf "$here/vimrc" $HOME/.vimrc
 vim +PlugInstall +qall
 
-mkdir -p $HOME/.config/ranger
-ln -sf "$here/ranger-rc.conf" $HOME/.config/ranger/rc.conf
-ln -sf "$here/ranger-scope.sh" $HOME/.config/ranger/scope.sh
-ln -sf "$here/ranger-commands.py" $HOME/.config/ranger/commands.py
-ln -sfT "$here/ranger-plugins" $HOME/.config/ranger/plugins/
-
 ln -sf "$here/config.fish" $HOME/.config/fish/config.fish
 ln -sfT "$here/fish_functions" $HOME/.config/fish/functions
 ln -sf "$here/bashrc" $HOME/.bashrc
