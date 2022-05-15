58a3afdfaf29d6bad300d5ce1dedfa7e09587e88
fiatjaf <faitjaf@gmail.com>
2022-05-14 21:44:39 -0300

    add qutebrowser settings.

---
+++ qutebrowser/config.py
@@ -0,0 +1,4 @@
+config.load_autoconfig(False)
+colors.webpage.preferred_color_scheme = "dark"
+config.bind(",v", "spawn mpv {url}")
+c.tabs.position = "bottom"
--- start.fish
+++ start.fish
@@ -1,11 +1,10 @@
 set here (pwd)
 
 ln -sfT "$here/vim" $HOME/.vim
-ln -sf "$here/nvim" "$HOME/.config/nvim"
-nvim +PlugInstall +qall
-
-ln -sf "$here/config.fish" $HOME/.config/fish/config.fish
+ln -sfT "$here/nvim" "$HOME/.config/nvim"
+ln -sfT "$here/qutebrowser" "$HOME/.config/qutebrowser"
 ln -sfT "$here/fish_functions" $HOME/.config/fish/functions
+ln -sf "$here/config.fish" $HOME/.config/fish/config.fish
 ln -sf "$here/bashrc" $HOME/.bashrc
 ln -sf "$here/mailcap" $HOME/.mailcap
 ln -sf "$here/muttrc" $HOME/.muttrc
@@ -18,3 +17,5 @@ ln -sf "$here/i3" $HOME/.i3/config
 ln -sf "$here/i3blocks" $HOME/.i3blocks.conf
 ln -sf "$here/xdefault " $HOME/.Xdefaults
 ln -sfT "$here/icons" $HOME/.icons
+
+nvim +PlugInstall +qall
