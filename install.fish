6b5f75780ed101b62ea9d789c3b0fe32453fe26a
fiatjaf <fiatjaf@gmail.com>
2024-09-28 20:33:14 -0300

    install graphviz.

--- install.fish
+++ install.fish
@@ -1,1 +1,1 @@
-pacman -S i3-wm yt-dlp wget curl git fish zip unzip xsel scrot the_silver_searcher fd ripgrep neovim moreutils redshift jq ncdu tree just broot pastel rofi i3blocks tmux entr fzf bat less alacritty openssh man-db btop peek net-tools
+pacman -S i3-wm yt-dlp wget curl git fish zip unzip xsel scrot the_silver_searcher fd ripgrep neovim moreutils redshift jq ncdu tree just broot pastel rofi i3blocks tmux entr fzf bat less alacritty openssh man-db btop peek net-tools graphviz
